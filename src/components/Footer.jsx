927e0edd90209ef50dbe38fe3b070f494b6c7f60
portable-ewaade <portableewaade@gmail.com>
2023-08-03 19:25:58 +0100

    review

---
+++ src/components/Footer.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='pt-5'>
+        <div className="container text-black">
+            <div className="row mt-3 ">
+                <div className="col-md-3">
+                    <a href="tel:+239035169253">
+                    <h6>+239035169253</h6>
+                    </a>
+                    <hr />
+                    <div className="w-50 my-4">
+                        <img src="../../../assets/icons/instagram.png" alt="..." className='mx-3 '
+                        style={{width:"1.5rem"}} />
+                        <img src="../../../assets/icons/facebook.png" alt="..." style={{width:"1.5rem"}}/>
+                    </div>
+                </div>
+                <div className="col-md-3">
+                    <a href="#services">
+                        <ul className='px-1' id='services'>Services</ul>
+                    </a>
+                    <a href="#">
+                        <ul className='px-1'>End of Tenancy</ul>
+                    </a>
+                    <a href="#">
+                        <ul className='px-1'>AirBnb Cleaning</ul>
+                    </a>
+                    
+                </div>
+                <hr className='d-md-none mt-4'/>
+                <div className="col-md-3">
+                    <a href="#">
+                        <ul className='px-1'>Coronavirus Cleaning</ul>
+                    </a>
+                    <a href="#">
+                        <ul className='px-1'>Company Let Accommodation Cleaning</ul>
+                    </a>
+                    <a href="#">
+                        <ul className='px-1'>Communal Area Cleaning</ul>
+                    </a>
+                   
+                </div>
+                <hr className='d-md-none mt-4'/>
+                <div className="col-md-3">
+                    <a href="/getStarted">
+                        <ul className='px-1'>Contact Us</ul>
+                    </a>
+                    <a href="#">
+                        <ul className='px-1'>News</ul>
+                    </a>
+                    <a href="#">
+                        <ul className='px-1'>Get Help</ul>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div className="bg-color">
+            <div className="container">
+                <div className="row mt-5 pt-md-4 pt-3 ">
+                    <div className="col-md-6">
+                        <p>Copyright &copy; Gray Cleaning 2023 </p>
+                    </div>
+                    <div className="col-md-6">
+                    <a href="#">
+                            <ul className='px-1'>
+                                Privacy Policy
+                                Terms & Conditions
+                            </ul>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
+
+{/* <a href="#" class="position-absolute position-fixed bottom-0 end-0 top-25 m-4 arrow-up">
+<i class="bi bi-arrow-up-circle h2"></i>
+</a> */}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+   <section>
+     <div className='container'>
+     <nav className="navbar navbar-expand-lg pt-2">
+     <div className="container-fluid">
+    <a class="navbar-brand" href="/">
+        <img src="../../../assets/colored-logo3.png" alt="..." className='img-fluid me-4'
+        style={{width:"6rem", height:"3rem"}}
+         />
+    </a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse mt-4" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0 fs-6 fw-medium ">
+        <li className="nav-item me-4 ">
+          <Link href="#services" class="nav-link text-color active font-weight-600"> Services
+          </Link>
+        </li>
+        <li className="nav-item me-4 ">
+          <Link href="#about" class="nav-link text-color active"> About
+          </Link>
+        </li>
+        <li className="nav-item me-4">
+          <Link href="/join" class="nav-link text-color active">Join Our Team
+          </Link>
+        </li>
+        <li className="nav-item me-4 ">
+          <a className="nav-link text-color " href="/reviews">Reviews</a>
+        </li>
+      </ul>
+         <button className="btn text-white bg-color fw-medium py-2 px-4 shadow-lg" type="submit">
+          <a href="/getStarted">Get Started</a>
+         </button>
+     </div>
+     </div>
+    </nav>
+    </div>
+   </section>
+  )
+}
+
+export default Navbar
